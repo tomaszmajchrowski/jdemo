c52dae21732b0bb51789f228bf18654ece104540
Tomasz Majchrowski <tomasz@mitlnx01>
2015-12-16 18:21:59 +0100

    junit test case added

---
+++ parent/hello/src/test/java/de/majchrowski/jdemo/MathTest.java
@@ -0,0 +1,26 @@
+package de.majchrowski.jdemo;
+
+import junit.framework.TestCase;
+
+public class MathTest extends TestCase {
+
+	public MathTest(String name) {
+		super(name);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public void testIsEven() {
+		/*int sampleA=5;
+		int sampleB=4;
+		boolean resA=Math.isEven(sampleA);
+		assertFalse('5 is not even', resA);*/
+	}
+
+}
