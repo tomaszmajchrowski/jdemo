cca5d224a4281c2624fbe2f31e3b03be8e4a86f5
Tomasz Majchrowski <tomasz@mitlnx01>
2015-12-16 17:53:17 +0100

    implementation added

---
+++ parent/hello/src/main/java/de/majchrowski/jdemo/Math.java
@@ -0,0 +1,16 @@
+package de.majchrowski.jdemo;
+
+public class Math {
+	
+	public boolean isEven(int number) {
+		if ((a mod 2) == 0)
+		{
+		    isEven = true;
+		}
+		else
+		{
+		    isEven = false;
+		}	
+	}
+
+}
