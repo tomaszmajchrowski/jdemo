e9b95551ff3da1fbfcac4433cc4abfed24310e5c
Tomasz Majchrowski <tomasz@mitlnx01>
2015-12-17 09:35:24 +0100

    backup purpose

--- parent/calc/src/main/java/de/majchrowski/jdemo/Calc.java
+++ parent/calc/src/main/java/de/majchrowski/jdemo/Calc.java
@@ -1,9 +1,29 @@
 package de.majchrowski.jdemo;
 
+import java.util.ArrayList;
+
 public class Calc {
 	
+	public Calc() {
+		   
+		}
+	
 	public int creSum(String inputStr) {
-		return 0;
+		
+		///ArrayList<int> outList = new parseInput<int>();
+		String[] aList= inputStr.split(",");
+		
+		/*outList=inpurStr.split(',');
+		
+		for ()
+		def sum=0;
+		list.forEach(sum+it );
+		*/
+		int sum=0;
+		for(String subStr : aList) {
+	        sum=sum+Integer.parseInt(subStr);
+	    }
+		return sum;
 	}
 
 }
